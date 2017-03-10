4bb9b8a3bb30db3cef4b6d18ee4af7c2c231fdba
Amanda Affolter <Amanda.affolter@atkearney.com>
2017-03-10 17:29:38 -0600

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,22 @@
+dataURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if (file.exists("../rawData/household_power_consumption.zip") ||  file.exists("../rawData/household_power_consumption.txt")) {
+  message("== Data file (zip or txt format) exists, skipping downloading step \r")
+  } else { if (! (file.exists("../rawData/"))) { 
+    message("== Creating data folder, warning message might show... \r")
+    dir.create("../rawData/")
+  }
+    message("== Downloading data file...\r")
+  download.file(url=dataURL, destfile = "../rawData/household_power_consumption.zip")
+}
+if (file.exists("../rawData/household_power_consumption.txt")) {message("== Data file (txt format) exists, skipping extraction...\r")
+  unzip(zipfile = "../rawData/household_power_consumption.zip", exdir="../rawData/")
+}
+data <- read.table(file = "../rawData/household_power_consumption.txt", sep = ";", header = FALSE,
+na.strings = "?", nrows = 2880, skip = 66637)
+colnames(data) <- c("date","time", "GlobalActivePower", "GlobalReactivePower", "Voltage", "GlobalIntensity",
+                    "SubMetering1", "SubMetering2", "SubMetering3")
+data$datetime <- strptime(paste(data$date, data$time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
+data$date <- as.Date(data$date, format = "%d/%m/%Y")
+png (filename = "plot1.png", width = 480, height = 480)
+hist(data$GlobalActivePower, col = "red", main = "Global Active Power", xlab = "Global Active Power (Kilowatts)")
+dev.off()
